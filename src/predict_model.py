a484a096fc9a07cfa99dc68c9986ad188fd0940d
KinemuchiRyunosuke <kinemuchi1124@gmail.com>
2022-12-21 14:38:39 +0900

    update: GPU認識部分のコードを変更

--- src/predict_model.py
+++ src/predict_model.py
@@ -54,26 +54,10 @@ false_positive_path = "reports/result/false_positive.csv"
 positive_pred_path = "reports/result/positive_pred.csv"
 
 def main():
-    try:
-        tpu = tf.distribute.cluster_resolver.TPUClusterResolver() # TPU detection
-    except ValueError:
-        tpu = None
-        gpus = tf.config.experimental.list_logical_devices("GPU")
-
-    if tpu:
-        tf.tpu.experimental.initialize_tpu_system(tpu)
-        strategy = tf.distribute.experimental.TPUStrategy(tpu, steps_per_run=128) # Going back and forth between TPU and host is expensive. Better to run 128 batches on the TPU before reporting back.
-        print('Running on TPU ', tpu.cluster_spec().as_dict()['worker'])
-    elif len(gpus) > 1:
-        strategy = tf.distribute.MirroredStrategy([gpu.name for gpu in gpus])
-        print('Running on multiple GPUs ', [gpu.name for gpu in gpus])
-    elif len(gpus) == 1:
-        strategy = tf.distribute.get_strategy() # default strategy that works on CPU and single GPU
-        print('Running on single GPU ', gpus[0].name)
-    else:
-        strategy = tf.distribute.get_strategy() # default strategy that works on CPU and single GPU
-        print('Running on CPU')
-    print("Number of accelerators: ", strategy.num_replicas_in_sync)
+    gpus = tf.config.list_physical_devices(device_type='GPU')
+    if len(gpus) > 0:
+        print(f">> GPU detected. {gpus[0].name}")
+        tf.config.experimental.set_memory_growth(gpus[0], True)
 
     with open(motif_data_path, 'r') as f:
         motif_data = json.load(f)
@@ -141,8 +125,7 @@ def main():
         write_tfrecord(x_test, y_test, test_tfrecord_path)
         write_tfrecord(x_train, y_train, train_tfrecord_path)
 
-    with strategy.scope():
-        model = create_model()
+    model = create_model()
 
     if not os.path.exists(checkpoint_path):
         print("================== TRAINING ========================")
